586e6d66becebe0dbe30494a0e392a586ba433d5
corey szopinski <corey@core-industries.com>
2016-09-16 18:03:03 -0700

    adding -no-cache to build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/questions/22030931/how-to-rebuild-dockerfile-quick-by-using-cache/22089946#22089946
 # get squid ip to use in docker build
 SQUID_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' squid)
-docker build –no-cache --build-arg http_proxy=http://$SQUID_IP:3128 --build-arg https_proxy=https://$SQUID_IP:3128 -t coreindustries/neuralstyle .
+docker build --no-cache=true --build-arg http_proxy=http://$SQUID_IP:3128 --build-arg https_proxy=https://$SQUID_IP:3128 -t coreindustries/neuralstyle .
 # docker build -t coreindustries/neuralstyle .
