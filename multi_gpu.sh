dca003e3ebe2b6a3bce501e14d0e156ed8697fe5
corey szopinski <corey@core-industries.com>
2017-05-10 17:07:59 -0700

    adding multi-gpu

--- multi_gpu.sh
+++ multi_gpu.sh
@@ -10,19 +10,19 @@ OUTPUT=/projects/photos/output
 STYLE_WEIGHT=5e2
 STYLE_SCALE=1.0
 
-# clear;time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/th neural_style.lua \
-#   -content_image $CONTENT_IMAGE \
-#   -style_image $STYLE_IMAGE \
-#   -style_scale $STYLE_SCALE \
-#   -print_iter 1 \
-#   -style_weight $STYLE_WEIGHT \
-#   -image_size 256 \
-#   -output_image $OUTPUT/out1.png \
-#   -tv_weight 0 \
-#   -backend cudnn -cudnn_autotune
+clear;time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/th neural_style.lua \
+  -content_image $CONTENT_IMAGE \
+  -style_image $STYLE_IMAGE \
+  -style_scale $STYLE_SCALE \
+  -print_iter 1 \
+  -style_weight $STYLE_WEIGHT \
+  -image_size 256 \
+  -output_image $OUTPUT/out1.png \
+  -tv_weight 0 \
+  -backend cudnn -cudnn_autotune
 
 echo "------"
-time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/thneural_style.lua \
+time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/th thneural_style.lua \
   -content_image $CONTENT_IMAGE \
   -style_image $STYLE_IMAGE \
   -init image -init_image $OUTPUT/out1.png \
@@ -35,46 +35,49 @@ time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neur
   -tv_weight 0 \
   -backend cudnn -cudnn_autotune
 
-# clear;time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/thneural_style.lua \
-#   -content_image $CONTENT_IMAGE \
-#   -style_image $STYLE_IMAGE \
-#   -init image -init_image $OUTPUT/out2.png \
-#   -style_scale $STYLE_SCALE \
-#   -print_iter 1 \
-#   -style_weight $STYLE_WEIGHT \
-#   -image_size 1024 \
-#   -num_iterations 200 \
-#   -output_image $OUTPUT/out3.png \
-#   -tv_weight 0 \
-#   -backend cudnn -cudnn_autotune
+echo "------"
+time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/th thneural_style.lua \
+  -content_image $CONTENT_IMAGE \
+  -style_image $STYLE_IMAGE \
+  -init image -init_image $OUTPUT/out2.png \
+  -style_scale $STYLE_SCALE \
+  -print_iter 1 \
+  -style_weight $STYLE_WEIGHT \
+  -image_size 1024 \
+  -num_iterations 200 \
+  -output_image $OUTPUT/out3.png \
+  -tv_weight 0 \
+  -backend cudnn -cudnn_autotune
 
-# clear;time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/thneural_style.lua \
-#   -content_image $CONTENT_IMAGE \
-#   -style_image $STYLE_IMAGE \
-#   -init image -init_image $OUTPUT/out3.png \
-#   -style_scale $STYLE_SCALE \
-#   -print_iter 1 \
-#   -style_weight $STYLE_WEIGHT \
-#   -image_size 2048 \
-#   -num_iterations 100 \
-#   -output_image $OUTPUT/out4.png \
-#   -tv_weight 0 \
-#   -gpu 0,1 \
-#   -backend cudnn
+echo "------"
+time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/th thneural_style.lua \
+  -content_image $CONTENT_IMAGE \
+  -style_image $STYLE_IMAGE \
+  -init image -init_image $OUTPUT/out3.png \
+  -style_scale $STYLE_SCALE \
+  -print_iter 1 \
+  -style_weight $STYLE_WEIGHT \
+  -image_size 2048 \
+  -num_iterations 100 \
+  -output_image $OUTPUT/out4.png \
+  -tv_weight 0 \
+  -gpu 0,1 \
+  -backend cudnn
 
-# clear;time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/th neural_style.lua \
-#   -content_image $CONTENT_IMAGE \
-#   -style_image $STYLE_IMAGE \
-#   -init image -init_image $OUTPUT/out4.png \
-#   -style_scale $STYLE_SCALE \
-#   -print_iter 1 \
-#   -style_weight $STYLE_WEIGHT \
-#   -image_size 3620 \
-#   -num_iterations 50 \
-#   -save_iter 25 \
-#   -output_image $OUTPUT/out5.png \
-#   -tv_weight 0 \
-#   -lbfgs_num_correction 5 \
-#   -gpu 0,1 \
-#   -multigpu_strategy 6,12 \
-#   -backend cudnn
+echo "------"
+time nvidia-docker run -i -v /mnt/raid/projects:/projects -t coreindustries/neuralstyle:latest /opt/torch/install/bin/th neural_style.lua \
+  -content_image $CONTENT_IMAGE \
+  -style_image $STYLE_IMAGE \
+  -init image -init_image $OUTPUT/out4.png \
+  -style_scale $STYLE_SCALE \
+  -print_iter 1 \
+  -style_weight $STYLE_WEIGHT \
+  -image_size 3620 \
+  -num_iterations 50 \
+  -save_iter 25 \
+  -output_image $OUTPUT/out5.png \
+  -tv_weight 0 \
+  -lbfgs_num_correction 5 \
+  -gpu 0,1 \
+  -multigpu_strategy 6,12 \
+  -backend cudnn
